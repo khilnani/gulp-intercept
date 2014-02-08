84539ef4cb03fa233dec2aa4094c4cfe00719215
Nik <nik@khilnani.org>
2014-02-08 02:49:58 -0500

    Create main.js

---
+++ test/main.js
@@ -0,0 +1,11 @@
+var intercept = require('../');
+require('mocha');
+
+describe('intercept', function() {
+  describe('intercept()', function() {
+    it('should replace the file  on a stream', function(done) {
+
+      
+    });
+  });
+});
